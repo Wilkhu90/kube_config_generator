cfe30c39fc4c44d62e57a3d133facde0ac2b7017
Sumeet Wilkhu <sumeet.wilkhu@gmail.com>
2019-08-02 19:08:35 -0400

    Update generate_kube_config.sh

--- generate_kube_config.sh
+++ generate_kube_config.sh
@@ -4,7 +4,6 @@ namespace=$3
 
 kubectl create namespace $namespace
 sed -i 's/$NAMESPACE/'$namespace'/g' access.yaml
-sed -i 's/$NAMESPACE/'$namespace'/g' kube_config.yaml
 kubectl apply -f access.yaml
 
 sa_secret_token_name=$(kubectl get secret -n $namespace | grep "$namespace-user-token" | cut -d ' ' -f 1)
@@ -16,6 +15,7 @@ sed -i 's/$KUBERNETES_API_ENDPOINT/'$kubernetes_api_endpoint'/g' kube_config.yam
 sed -i 's/$CERTIFICATE/'$certificate'/g' kube_config.yaml
 sed -i 's/$TOKEN/'$token'/g' kube_config.yaml
 sed -i 's/$CLUSTER_NAME/'$cluster_name'/g' kube_config.yaml
+sed -i 's/$NAMESPACE/'$namespace'/g' kube_config.yaml
 
 echo "Please place the output in /root/.kube/config file"
 cat kube_config.yaml
