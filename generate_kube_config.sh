adb22daa8e73c74f04c8031e0907328d05078b3a
Sumeet Wilkhu <sumeet.wilkhu@gmail.com>
2019-08-01 21:32:31 -0400

    first commit

--- generate_kube_config.sh
+++ generate_kube_config.sh
@@ -6,8 +6,10 @@ sed -i 's/$NAMESPACE/'$namespace'/g' access.yaml
 sed -i 's/$NAMESPACE/'$namespace'/g' kube_config.yaml
 kubectl apply -f access.yaml
 
-token=$(kubectl get secret spinnaker-user-token-xxxxx -n spinnaker -o "jsonpath={.data.token}" | base64 -D)
-certificate=$(kubectl get secret spinnaker-user-token-xxxxx -n spinnaker -o "jsonpath={.data['ca\.crt']}")
+sa_secret_token_name=$(kubectl get secret -n $namespace | grep "$namespace-user-token" | cut -d ' ' -f 1)
+
+token=$(kubectl get secret $sa_secret_token_name -n $namespace -o "jsonpath={.data.token}" | base64 -D)
+certificate=$(kubectl get secret $sa_secret_token_name -n $namespace -o "jsonpath={.data['ca\.crt']}")
 
 sed -i 's/$KUBERNETES_API_ENDPOINT/'$kubernetes_api_endpoint'/g' kube_config.yaml
 sed -i 's/$CERTIFICATE/'$certificate'/g' kube_config.yaml
