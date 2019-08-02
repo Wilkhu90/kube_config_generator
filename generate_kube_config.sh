806b478cf6d79883d390783e5501b02dcedebce6
Sumeet Wilkhu <sumeet.wilkhu@gmail.com>
2019-08-02 19:09:46 -0400

    Update generate_kube_config.sh

--- generate_kube_config.sh
+++ generate_kube_config.sh
@@ -12,10 +12,10 @@ token=$(kubectl get secret $sa_secret_token_name -n $namespace -o "jsonpath={.da
 certificate=$(kubectl get secret $sa_secret_token_name -n $namespace -o "jsonpath={.data['ca\.crt']}")
 
 sed -i 's/$KUBERNETES_API_ENDPOINT/'$kubernetes_api_endpoint'/g' kube_config.yaml
-sed -i 's/$CERTIFICATE/'$certificate'/g' kube_config.yaml
-sed -i 's/$TOKEN/'$token'/g' kube_config.yaml
 sed -i 's/$CLUSTER_NAME/'$cluster_name'/g' kube_config.yaml
 sed -i 's/$NAMESPACE/'$namespace'/g' kube_config.yaml
+sed -i 's/$TOKEN/'$token'/g' kube_config.yaml
+sed -i 's/$CERTIFICATE/'$certificate'/g' kube_config.yaml
 
 echo "Please place the output in /root/.kube/config file"
 cat kube_config.yaml
